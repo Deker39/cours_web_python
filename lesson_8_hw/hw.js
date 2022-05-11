4955f349344631cf3e9f03dd27fd562d3f87bcef
Alexey Golenko <bigtigerlesha@gmail.com>
2022-05-11 12:57:16 +0300

    js1

--- lesson_8_hw/hw.js
+++ lesson_8_hw/hw.js
@@ -1,13 +1,64 @@
 //promt()
 //alert()
 function one_exer(){
-    let kek = prompt("Введи число")
-    alert(kek**2)
+    let kek = Number(prompt("Введи число"))
+    alert("Степень вашего числа = " + kek**2)
 }
 function two_exer(){
-    let kek1 = prompt("Введи число 1")
-    let kek2 = prompt("Введи число 2")
-    result = (kek1+ kek2)/2
-    alert(result)
+    let kek1 = Number(prompt("Введи число 1"))
+    let kek2 = Number(prompt("Введи число 2"))
+    alert("Среднее арефметическое двух = " + (kek1+ kek2)/2)
 }
-two_exer()
+function three_exer(){
+    let kek = Number(prompt("Введи сторону квадрата"))
+    alert("Площадь квадрата = "+kek**2)
+}
+function four_exer(){
+    let kek = Number(prompt("Введите километры"))
+    let result= kek*0.621371
+    alert("В милях это примерно= "+ Math.ceil(result))
+}
+function five_exer(){
+    let kek1 = Number(prompt("Введи число 1"))
+    let kek2 = Number(prompt("Введи число 2"))
+    let result_plus = kek1 + kek2
+    let result_minus = kek1 - kek2
+    let result_multiply = kek1 * kek2
+    let result_divide = kek1 / kek2
+    alert("Сумма "+ result_plus + "\n" +
+          "Разность "+ result_minus + "\n" +
+          "Произвидение "+ result_multiply + "\n" +
+          "Частное "+ result_multiply
+        )
+}
+function six_exer(){
+    let kek1 = Number(prompt("Введи число a"))
+    let kek2 = Number(prompt("Введи число b"))
+    let result = (-kek2)/kek1
+    alert("a*x+b=0 нужно найти X \n"+ "x ="+ result)
+}
+function seven_exer(){
+    let kek1 = Number(prompt("Введи сколько часов"))
+    let kek2 = Number(prompt("Введи сколько минут"))
+    let now = new Date().toLocaleTimeString().slice(0,-3)
+    let result1 = 23-kek1 
+    let result2 = 60-kek2
+    alert( result1+":"+ result2);
+}
+function eight_exer(){
+    let kek1 = Number(prompt("Введи трехзначное число"))
+    letresult = Math.floor(kek1/10)%10
+    alert("Второе число " + result)
+}
+function nine_exer(){
+    let kek1 = prompt("Введи пятизначное число")
+    let result1 = kek1%10
+    let result2 = Math.floor(kek1/10)
+    alert(result1 + "" + result2)
+}
+function ten_exer(){
+    let kek =  prompt("Общаяя сумма продаж за месяц")
+    let result = 250 + (kek * 0.1)
+    alert("Ваша зарплата " + result + "$")
+}
+ten_exer()
