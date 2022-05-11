090a43c5d5971ea20e413fedf302051dfbd8ac9e
Alexey Golenko <alexey.holenko@gmail.com>
2022-05-11 11:02:29 +0300

    8

---
+++ lesson_8_hw/hw.js
@@ -0,0 +1,13 @@
+//promt()
+//alert()
+function one_exer(){
+    let kek = prompt("Введи число")
+    alert(kek**2)
+}
+function two_exer(){
+    let kek1 = prompt("Введи число 1")
+    let kek2 = prompt("Введи число 2")
+    result = (kek1+ kek2)/2
+    alert(result)
+}
+two_exer()
