76747f7c5e6914dc75e4c76c602453f5f407942a
Alexey Golenko <bigtigerlesha@gmail.com>
2022-05-26 12:42:11 +0300

    hw12

--- lesson_12_hw/hw1.js
+++ lesson_12_hw/hw1.js
@@ -1,3 +1,5 @@
+/*-----------------------------------------------*/
+
 let p = document.getElementById('name')
 
 p.addEventListener('keydowm',function(e){
@@ -7,4 +9,23 @@ p.addEventListener('keydowm',function(e){
 p.addEventListener('input', function(e){
   p.value = p.value.replace(/[0-9]/g, "");
 });
+
+/*-----------------------------------------------*/
+
+  function foo(){
+    let b = document.getElementById('open')
+    let c = document.getElementById('close')
+    let win = document.getElementById('myModal')
+    document.addEventListener('click', function(e) {
+      if (b){
+        window.open('modal_win.html')
+            
+      }
+      if(c){
+        window.open('hw1.html')
+      }
+  
+    })
+  }
+ 
 
