1cc4398097b87bb1cb61c40071e68b53bd748b5e
Alexey Golenko <alexey.holenko@gmail.com>
2022-06-18 16:57:50 +0300

    12

--- practice_12_js/pw.js
+++ practice_12_js/pw.js
@@ -29,6 +29,7 @@ function sixth(array,index){
 let arr = Array.from({length: 10}, () => Math.floor(Math.random() * 40));
 let indexChanged = 5
 let indexDeleted = 0
+
 first(arr)
 console.log(`Even elements in array: ${second(arr)}`);
 console.log(`Sum of all array elements: ${thirth(arr)}`);
@@ -37,3 +38,19 @@ console.log(`Item changed: ${indexChanged+1} [${fifth(arr,5,34)}]`);
 first(arr)
 sixth(arr,indexDeleted)
 first(arr)
+/*-------------------------------------*/   
+function seventh(arr1,arr2) {
+    arr1.concat(arr2).forEach(function(i){
+        console.log(`i: ${i}`);
+        arr2.forEach(e => console.log(e) )
+        // arr_jointed[i] = true
+    })
+    // let result = Object.keys(arr_jointed);
+    // console.log(result);
+}
+let arr_jointed = []
+let arr_one = Array.from({length: 5}, () => Math.floor(Math.random() * 40));
+let arr_two = Array.from({length: 5}, () => Math.floor(Math.random() * 40));
+console.log(arr_one);
+console.log(arr_two);
+seventh(arr_one,arr_two)
