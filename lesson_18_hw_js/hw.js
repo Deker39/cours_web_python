15ec812370e8008ec51d4ff1d9e3c8ed09a271dd
Alexey Golenko <alexey.holenko@gmail.com>
2022-06-20 23:24:29 +0300

    18

--- lesson_18_hw_js/hw.js
+++ lesson_18_hw_js/hw.js
@@ -1,4 +1,12 @@
 var colorReg = RegExp(/^[a-zA-Z]+$/gm)
 var rgbReg  = RegExp(/(\d{1,3}),(\d{1,3}),(\d{1,3})/gm)
-var rgbareg = RegExp(/(\d{1,3}),(\d{1,3}),(\d{1,3}),([0]*\.?[0-9]|1)/g)
-var hexReg = RegExp()
+var rgbareg = RegExp(/(\d{1,3}),(\d{1,3}),(\d{1,3}),(1|1\.?\.0|0\.?\.\d{0,}|0)/gm )
+var hexReg = RegExp(/#[0-9A-Fa-f]{6}/mg)
+
+
+var mainDiv = document.getElementById('view')
+var creatDiv = document.createElement('div')
+var colorName = document.getElementById('colorId')
+var colorType = document.getElementById('typeId')
+var colorCode = document.getElementById('codeId')
+console.log(colorName.textContent);
