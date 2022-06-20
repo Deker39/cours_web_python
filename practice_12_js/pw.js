51061b6fd55ac7a58ffe3a6e2e91e11674c1739c
Alexey Golenko <alexey.holenko@gmail.com>
2022-06-20 10:39:35 +0300

    20

--- practice_12_js/pw.js
+++ practice_12_js/pw.js
@@ -39,18 +39,89 @@ first(arr)
 sixth(arr,indexDeleted)
 first(arr)
 /*-------------------------------------*/   
+
 function seventh(arr1,arr2) {
-    arr1.concat(arr2).forEach(function(i){
-        console.log(`i: ${i}`);
-        arr2.forEach(e => console.log(e) )
-        // arr_jointed[i] = true
-    })
-    // let result = Object.keys(arr_jointed);
-    // console.log(result);
+    arr_jointed = arr1.concat(arr2)
+    return arr_jointed = arr_jointed.filter(function(item,pos) {return arr_jointed.indexOf(item) == pos}) 
+    // return [...new Set([...arr1 ,...arr2])]
 }
+function eighth(arr1,arr2){
+    var a_values = Object.keys(arr1).map(function(key){
+        return arr1[key];
+    });
+    
+    var b_values = Object.keys(arr2).map(function(key){
+        return arr2[key];
+    });
+    for (var a_key in a_values) {
+        if (b_values.indexOf(a_values[a_key]) != -1) {
+           arr_repetition.push(a_values[a_key])
+        }
+    }
+    return arr_repetition
+  
+   
+}
+function nineth(arr1,arr2){
+    var a_values = Object.keys(arr1).map(function(key){
+        return arr1[key];
+    });
+    
+    var b_values = Object.keys(arr2).map(function(key){
+        return arr2[key];
+    });
+
+    for (var a_key in a_values) {
+        if (b_values.indexOf(a_values[a_key]) == -1) {
+            arr_remove.push(a_values[a_key])
+        }
+    }
+    return arr_remove
+}
+
 let arr_jointed = []
+let arr_remove = []
+let arr_repetition = []
 let arr_one = Array.from({length: 5}, () => Math.floor(Math.random() * 40));
 let arr_two = Array.from({length: 5}, () => Math.floor(Math.random() * 40));
 console.log(arr_one);
 console.log(arr_two);
-seventh(arr_one,arr_two)
+console.log(`All elements from two arrays without repetition ${seventh(arr_one,arr_two)}`);
+console.log(`Common elements without repetition: ${eighth(arr_one,arr_two)}`);
+console.log(`All elements from the first array, which are not in the second array: ${nineth(arr_one,arr_two)}`);
+/*-------------------------------------*/ 
+
+var fruit = ['apricot', 'quince', 'orange', 'watermelon', 'banana', 'grape', 'pomegranate', 'grapefruit', 'pear', 'guava', 
+            'melon', 'fig', 'cantaloupe','apple', 'carambola', 'kiwi', 'lemon', 'mango', 'marania', 'medlar', 'pepino', 'peach', 
+            'pitaya', 'pomelo', 'physalis', 'persimmon']
+
+let cont = document.createElement('section')
+let ul = document.createElement('ul')
+
+document.body.appendChild(cont)
+cont.appendChild(ul)
+
+cont.classList.add('container')
+ul.classList.add('list-group','list-group-flush')
+
+for( var i = 0; i< fruit.length;i++){
+    var li = document.createElement('li')
+    ul.appendChild(li)
+    li.classList.add('list-group-item')
+    li.innerHTML = li.innerHTML + fruit.sort()[i]
+}
+
+function tenth(nameFruit){
+    alert(`Index of your fruit: ${fruit.indexOf(nameFruit)}`)
+    return fruit.indexOf(nameFruit)
+}
+
+ul.addEventListener('click', function(e){
+    e.target.classList.toggle('bg-danger')
+    console.log(`Index of your fruit: ${fruit.indexOf(e.target.textContent)}`);
+    
+})
+ul.addEventListener('mouseout',function(e) {
+    e.target.classList.remove('bg-danger')
+})
+
