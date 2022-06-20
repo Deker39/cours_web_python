3be237fec606835d8d34b8e58abab301a4599887
Alexey Golenko <alexey.holenko@gmail.com>
2022-06-20 22:05:18 +0300

    18

---
+++ lesson_18_hw_js/hw.js
@@ -0,0 +1,4 @@
+var colorReg = RegExp(/^[a-zA-Z]+$/gm)
+var rgbReg  = RegExp(/(\d{1,3}),(\d{1,3}),(\d{1,3})/gm)
+var rgbareg = RegExp(/(\d{1,3}),(\d{1,3}),(\d{1,3}),([0]*\.?[0-9]|1)/g)
+var hexReg = RegExp()
