f86c30adeba30a6aab89fe92f029140aa7711bb9
Alexey Golenko <alexey.holenko@gmail.com>
2022-06-22 23:42:24 +0300

    18 valid done

--- lesson_18_hw_js/hw.js
+++ lesson_18_hw_js/hw.js
@@ -20,20 +20,22 @@ let form = document.querySelector('#form'),
     colorError  = document.querySelector('#colorError'),
     typeError = document.querySelector('#typeError'),
     codeError = document.querySelector('#codeError'),
-    divViewColor = document.querySelector('#view')
+    divViewColor = document.querySelector('#view'),
+    arrayName = new Array()
+   
     
 function validtateColorName(name){
-    let reg = /[a-zA-Z]/
+    let reg = /^[a-zA-Zа-яА-ЯёЁ]+$/g
     return reg.test(name)
 }
 
 function validtateRGB(code){
-    let reg = /(\d{1,3}),(\d{1,3}),(\d{1,3})/
+    let reg = /^\d{1,3}[\s,]\d{1,3}[\s,]\d{1,3}$/
     return reg.test(code)
 }
 
 function validtateRGBA(code){
-    let reg = /(\d{1,3}),(\d{1,3}),(\d{1,3}),(1|1\.?\.0|0\.?\.\d{0,}|0)/
+    let reg =  /^\d{1,3}[\s,]\d{1,3}[\s,]\d{1,3}[\s,](0.[0-9]+|[0-1])$/
     return reg.test(code)
 }
 
@@ -43,10 +45,12 @@ function validtateHEX(code){
 }
 
 form.onsubmit  = function(){
-    let colorNameValue = inputColorName.value,
-        colorTypeValue = inputColorType.value,
-        colorCodeValue = inputColorCode.value
-
+    let colorNameValue = inputColorName.value.toLowerCase(),
+        colorTypeValue = inputColorType.value.toLowerCase(),
+        colorCodeValue = inputColorCode.value.toLowerCase(),
+        backValue = new String()
+       
+// переделать на функции все так 
     // console.log(colorNameValue,colorTypeValue,colorCodeValue);
 
     formInputs.forEach(function (input){
@@ -59,17 +63,16 @@ form.onsubmit  = function(){
         
     });
 
-    if (!validtateColorName(colorNameValue)){
-        inputColorName.classList.add('is-invalid')
-        colorError.classList.remove('display-none')
-        console.log('Name not valid')
-        // return false
+
+
+
+    if(!document.querySelector('#colorError p')){
+        var pName = document.createElement('p')
+        pName.classList.add('text-danger','mb-0','fs-9')
+        colorError.appendChild(pName)
     }else{
-        colorError.classList.add('display-none')
-        inputColorName.classList.remove('is-invalid') 
-        inputColorName.classList.add('is-valid') 
-       
-    } 
+        pName = document.querySelector('#colorError p')
+    }
 
     if(!document.querySelector('#typeError p')){
         var pType = document.createElement('p')
@@ -86,6 +89,31 @@ form.onsubmit  = function(){
     }else{
         pCode = document.querySelector('#codeError p')
     }
+
+       
+
+    if (!validtateColorName(colorNameValue)){
+        inputColorName.classList.add('is-invalid')
+        colorError.classList.remove('display-none')
+
+        pName.innerText = 'Color can only contain letters'
+        console.log('Name not valid')
+        // return false
+    }else{
+        if(arrayName.includes(colorNameValue)){
+            inputColorName.classList.add('is-invalid')
+            colorError.classList.remove('display-none')
+            pName.innerText = 'This name is already in use'
+            console.log('Name is already in use')
+            return false
+        }else{
+            colorError.classList.add('display-none')
+            inputColorName.classList.remove('is-invalid') 
+            inputColorName.classList.add('is-valid') 
+        }
+       
+       
+    } 
    
     
     if (colorTypeValue == "") {
@@ -116,6 +144,7 @@ form.onsubmit  = function(){
                 codeError.classList.add('display-none')
                 inputColorCode.classList.remove('is-invalid')
                 inputColorCode.classList.add('is-valid')
+                backValue = `${colorTypeValue}(${colorCodeValue})`
             }
 
         }
@@ -131,6 +160,7 @@ form.onsubmit  = function(){
                 codeError.classList.add('display-none')
                 inputColorCode.classList.remove('is-invalid')
                 inputColorCode.classList.add('is-valid')
+                backValue = `${colorTypeValue}(${colorCodeValue})`
             }
         }
         if (colorTypeValue == 'hex') {
@@ -145,26 +175,55 @@ form.onsubmit  = function(){
                 codeError.classList.add('display-none')
                 inputColorCode.classList.remove('is-invalid')
                 inputColorCode.classList.add('is-valid')
+                backValue = `${colorCodeValue}`
             }
         }
+
+        
+    }
+
+    if(validtateColorName(colorNameValue) && colorTypeValue != "" && 
+    (validtateRGB(colorCodeValue)||validtateRGBA(colorCodeValue)||validtateHEX(colorCodeValue))){
+        
+        var divItemColor = document.createElement('div'),
+            infoDivItem = document.createElement('div'),
+            pInfoName = document.createElement('p'),
+            pInfoType = document.createElement('p'),
+            pInfoCode = document.createElement('p')
+
+        divViewColor.appendChild(divItemColor)
+        divItemColor.appendChild(infoDivItem)
+        infoDivItem.appendChild(pInfoName,pInfoType,pInfoCode)
+        
+        divItemColor.classList.add('m-3','cont-color',"d-flex", "justify-content-center","align-items-center")
+        infoDivItem.classList.add("d-flex", "flex-column" ,"justify-content-center","align-items-center","cont-into-color")
+        pInfoName.classList.add("fs-4", "fw-bold")
+        pInfoCode.classList.add("fs-4")
+
+        
+        divItemColor.style.backgroundColor = backValue  
+        infoDivItem.style.backgroundColor = 'rgba(225, 225, 225, 0.25)'
+        pInfoName.innerHTML = colorNameValue.toUpperCase()
+        pInfoType.innerHTML = colorTypeValue.toUpperCase()
+        pInfoCode.innerHTML = `(${colorCodeValue})`
+        infoDivItem.appendChild(pInfoName)
+        infoDivItem.appendChild(pInfoType)
+        infoDivItem.appendChild(pInfoCode)
+        
+        arrayName.push(colorNameValue)
+        console.log(arrayName);
+        document.cookie = ``
+        
+
+
+        
+       return false
     }
 
     if(emptyInputs.length == 0){
         console.log('input not filled');
         return false
     }
-
-    if(validtateColorName(colorNameValue) && colorTypeValue != "" && 
-    (validtateRGB(colorCodeValue)||validtateRGBA(colorCodeValue)||validtateHEX(colorCodeValue))){
-        console.log("alina BUbka");
-        var divItemColor = document.createElement('div')
-        divItemColor.classList.add('m-3','cont-color')
-        // divViewColor
-        
-    }
-
-
-
 
     return false
 }
