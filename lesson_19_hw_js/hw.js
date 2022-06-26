66937c0a86c7b15df4b42004f03f9168a4326cfa
Alexey Golenko <alexey.holenko@gmail.com>
2022-06-27 01:31:22 +0300

    19

--- lesson_19_hw_js/hw.js
+++ lesson_19_hw_js/hw.js
@@ -1,95 +1,173 @@
 let form = document.querySelector('#form'),
     inputTitle  = document.querySelector('#titleId'), 
     inputType  = document.querySelector('#typeId'),
+    typeError = document.querySelector('#typeError'),
+    titleError = document.querySelector('#titleError'),
     formInputs = document.querySelectorAll(".form-control"),
+    emptyInputs = Array.from(formInputs).filter(input => input.value === ''),
     sectFirst = document.querySelector('#section-first'),
-    btnSearch = document.querySelector('#search')
+    btnSearch = document.querySelector('#search'),
     token = '55f2a631',
-    arrayMovies = []
-
-let textList = document.createElement('h2'),
     sectMovieList = document.createElement('section')
+
+function creatDetailMovie(img,title,relesed,gener,country,director,writer,actors,awards){
+    let sectMovieInfo= document.createElement('section'),
+        contInfo = document.createElement('div'),
+        contImgInfo = document.createElement('div'),
+        contTextInfo = document.createElement('div'),
+        textTitle = document.createElement('p'),
+        textRelesed = document.createElement('p'),
+        textGener = document.createElement('p'),
+        textCountry = document.createElement('p'),
+        textDirectors = document.createElement('p'),
+        textWriter = document.createElement('p'),
+        textActors = document.createElement('p'),
+        textAwards = document.createElement('p')
     
-    textList.innerHTML = 'Films'
-    textList.classList.add('d-flex','justify-content-center')
-    sectFirst.appendChild(textList)  
+   
 
+    if (!document.querySelector('#secFilmInfo h2')) {
+        let textInfo = document.createElement('h2')
+        textInfo.innerHTML = 'Films info'
+        textInfo.classList.add('d-flex', 'justify-content-center', 'align-content-center')
+        sectMovieInfo.appendChild(textInfo)
+    } else {
+        textInfo = document.querySelector('section h2')
+    }
 
-function creatDetailMovie(){
+    sectMovieInfo.classList.add('container', 'mb-5', 'd-flex','flex-column','align-content-center')
+
+    document.body.appendChild(sectMovieInfo)
+    sectMovieInfo.setAttribute('id','secFilmInfo')
+
+    sectMovieInfo.appendChild(contInfo)
+    contInfo.appendChild(contImgInfo)
+    contInfo.appendChild(contTextInfo)
+    contTextInfo.appendChild(textTitle)
+    contTextInfo.appendChild(textRelesed)
+    contTextInfo.appendChild(textGener)
+    contTextInfo.appendChild(textCountry)
+    contTextInfo.appendChild(textDirectors)
+    contTextInfo.appendChild(textWriter)
+    contTextInfo.appendChild(textActors)
+    contTextInfo.appendChild(textAwards)
+
+    contInfo.classList.add('d-flex', 'flex-row',  'mb-3', 'border', 'ms-3', 'bg-light')
+    contImgInfo.classList.add('m-2')
+    contTextInfo.classList.add('m-2')
+    textTitle.classList.add('fs-4')
+    textRelesed.classList.add('fs-4')
+    textGener.classList.add('fs-4')
+    textCountry.classList.add('fs-4')
+    textDirectors.classList.add('fs-4')
+    textWriter.classList.add('fs-4')
+    textTitle.classList.add('fs-4')
+    textActors.classList.add('fs-4')
+
+    contInfo.style.minWidth = "800px"
+    contInfo.style.height = "450px"
+    contImgInfo.style.minWidth  = "300px"
+    contImgInfo.style.minHeight = "400px"
+    if(img == 'N/A'){
+        contImgInfo.style.background = `url(no_film.png) center/cover no-repeat`
+        
+    }else{
+        contImgInfo.style.background = `url(${img}) center/cover no-repeat`
+    }
+
+    textTitle.innerText = `Title: \t${title}`
+    textRelesed.innerText = `Relesed: \t${relesed}`
+    textGener.innerText = `Gener: \t${gener}`
+    textCountry.innerText = `Country: \t${country}`
+    textDirectors.innerText = `Directors: \t${director}`
+    textWriter.innerText = `Writer: \t${writer}`
+    textTitle.innerText = `Actors: \t${actors}`
+    textActors.innerText = `Awards: \t${awards}`
+
 
 }
-function creatListMovies(img,name,type,year) {
 
+function creatListMovies(img,name,type,year) {
     
-    sectMovieList.classList.add('container', 'mb-5', 'd-flex', 'flex-wrap')
+    var contMovieItem = document.createElement('div'),
+        contImg = document.createElement('div'),
+        contTextItem = document.createElement('div'),
+        textType = document.createElement('p'),
+        textName = document.createElement('p'),
+        textYear = document.createElement('p'),
+        btnDetails = document.createElement('button')
+
+    sectMovieList.classList.add('container', 'mb-5', 'd-flex')
     document.body.appendChild(sectMovieList)
 
-    
-        var contMovieItem = document.createElement('div'),
-            contImg = document.createElement('div'),
-            contTextItem = document.createElement('div'),
-            textType = document.createElement('p'),
-            textName = document.createElement('p'),
-            textYear = document.createElement('p'),
-            btnDetails = document.createElement('button')
-        
-        sectMovieList.appendChild(contMovieItem)
-        contMovieItem.appendChild(contImg)
-        contMovieItem.appendChild(contTextItem)
-        contTextItem.appendChild(textType)
-        contTextItem.appendChild(textName)
-        contTextItem.appendChild(textYear)
-        contTextItem.appendChild(btnDetails)
 
-        contMovieItem.classList.add('d-flex', 'flex-row',  'mb-3', 'border', 'ms-3', 'bg-light')
-        contImg.classList.add('m-2')
-        contTextItem.classList.add('m-2', 'position-relative')
-        textName.classList.add('fw-bold')
-        btnDetails.classList.add('btn', 'btn-firts', 'btn-primary', 'position-absolute', 'bottom-0', 'end-0')
-        
-        contMovieItem.style.width = "320px"
-        contMovieItem.style.height = "270px"
-        contImg.style.minWidth  = "150px"
-        contImg.style.height = "250px"
-        if(img == 'N/A'){
-            contImg.style.background = `url(no_film.png) center/cover no-repeat`
-            
-        }else{
-            contImg.style.background = `url(${img}) center/cover no-repeat`
-        }
-       
-        textType.innerText = `Type: ${type}`
-        textName.innerText = `Title: ${name}`
-        textYear.innerText = `Year made: ${year}`
-        btnDetails.textContent = 'Details'
-
-        btnDetails.addEventListener('click', function(e){
-            detailRequest(name)// add url
-        });
-        // btnDetails.onclick = detailRequest(name)
+    if (!document.querySelector('#form + h2')) {
+        let textList = document.createElement('h2')
+        textList.innerHTML = 'Films'
+        textList.classList.add('d-flex', 'justify-content-center', 'align-content-center')
+        sectFirst.appendChild(textList)
+    } else {
+        textList = document.querySelector('#form + h2')
     }
+  
+    sectMovieList.appendChild(contMovieItem)
+    contMovieItem.appendChild(contImg)
+    contMovieItem.appendChild(contTextItem)
+    contTextItem.appendChild(textType)
+    contTextItem.appendChild(textName)
+    contTextItem.appendChild(textYear)
+    contTextItem.appendChild(btnDetails)
+
+    contMovieItem.classList.add('d-flex', 'flex-row',  'mb-3', 'border', 'ms-3', 'bg-light')
+    contImg.classList.add('m-2')
+    contTextItem.classList.add('m-2', 'position-relative')
+    textName.classList.add('fw-bold')
+    btnDetails.classList.add('btn', 'btn-firts', 'btn-primary', 'position-absolute', 'bottom-0', 'end-0')
+    
+    contMovieItem.style.minWidth = "320px"
+    contMovieItem.style.height = "270px"
+    contImg.style.minWidth  = "150px"
+    contImg.style.height = "250px"
+    if(img == 'N/A'){
+        contImg.style.background = `url(no_film.png) center/cover no-repeat`
+        
+    }else{
+        contImg.style.background = `url(${img}) center/cover no-repeat`
+    }
+    
+    textType.innerText = `Type: ${type}`
+    textName.innerText = `Title: ${name}`
+    textYear.innerText = `Year made: ${year}`
+    btnDetails.textContent = 'Details'
+
+    btnDetails.addEventListener('click', function(e){
+        detailRequest(name)// add url
+    });
+    
+}
 
 function detailRequest(name){
     let request
-    url = `https://www.omdbapi.com/?t=${name.replace(/ /g,'+')}&apikey=${token}`
-    console.log(url);
+    url_item = `https://www.omdbapi.com/?t=${name.replace(/ /g,'+')}&apikey=${token}`
+    console.log(url_item);
     if (window.XMLHttpRequest) {
         request = new XMLHttpRequest();
     }
     else {
         request = new ActiveXObject("Microsoft.XMLHTTP");
     }
-    request.open('GET', url)
+    request.open('GET', url_item)
     request.responseType = "json";
-    if (request.readyState === 4 &&request.status == 200) {
-        let person = request.response;
-        console.log(person);
-        
-        
+    request.onload = function () {
+        if (request.readyState === 4 &&request.status == 200) {
+            let person = request.response;
+            console.log(person);
+            creatDetailMovie(person['Poster'],person['Title'],person['Released'],person['Genre'],person['Country'],
+                person['Director'],person['Writer'],person['Actors'],person['Awards'])
+        }
     }
-
-request.send();
-return false
+    request.send();
+    return false
 }
 
 form.onsubmit = function(){
@@ -97,7 +175,48 @@ form.onsubmit = function(){
         typeValue = inputType.value.toLowerCase(),
         request
 
-    url = `https://www.omdbapi.com/?s=${titleValue.replace(/ /g,'+')}&sype=${typeValue}&apikey=${token}`
+
+    formInputs.forEach(function (input){
+        if(input.value === '' || input.value === ' '){
+            input.classList.add('is-invalid')
+        }else{
+            input.classList.remove('is-invalid')
+
+        }
+        
+    });
+
+    if(!document.querySelector('#titleError p')){
+        var pTittle = document.createElement('p')
+        pTittle.classList.add('text-danger','mb-0','fs-9')
+        titleError.appendChild(pTittle)
+    }else{
+        pTittle = document.querySelector('#titleError p')
+    }
+
+    if(!document.querySelector('#typeError p')){
+        var pType = document.createElement('p')
+        pType.classList.add('text-danger','mb-0','fs-9')
+        typeError.appendChild(pType)
+    }else{
+        pType = document.querySelector('#typeError p')
+    }
+
+    if (typeValue == "") {
+
+        pType.innerText = 'Need to select the type'
+
+        typeError.classList.remove('display-none')
+        inputType.classList.add('is-invalid')
+        console.log('Type not valid')
+        return false
+    }else{
+        typeError.classList.add('display-none')
+        inputType.classList.add('is-valid')
+    }
+
+
+    url_list = `https://www.omdbapi.com/?s=${titleValue.replace(/ /g,'+')}&type=${typeValue}&apikey=${token}`
     // console.log(url);
     
     if (window.XMLHttpRequest) {
@@ -106,38 +225,42 @@ form.onsubmit = function(){
     else {
         request = new ActiveXObject("Microsoft.XMLHTTP");
     }
-    request.open('GET', url)
+    request.open('GET', url_list)
     request.responseType = "json";
     request.onload = function () {
         if (request.readyState === 4 &&request.status == 200) {
             let person = request.response;
-            for(var i = 0; i < person['Search']['length']; i++){
-                creatListMovies(person['Search'][i]['Poster'],person['Search'][i]['Title'],
-                                person['Search'][i]['Type'],person['Search'][i]['Year'])//creatListMovies(img,name,type,year)
+            console.log(person);
+            if(person['Response'] == 'False'){
+                pTittle.innerText = 'No such movie'
+                titleError.classList.remove('display-none')
+                inputTitle.classList.add('is-invalid')
+                console.log('No such movie')
+                return false
+            }else{
+                titleError.classList.add('display-none')
+                inputTitle.classList.add('is-valid')
+                for(var i = 0; i < person['Search']['length']; i++){
+                    creatListMovies(person['Search'][i]['Poster'],person['Search'][i]['Title'],
+                                    person['Search'][i]['Type'],person['Search'][i]['Year'])//creatListMovies(img,name,type,year)
+                }
             }
+            
             
         }
     }
     request.send();
+
+
+    if(emptyInputs.length == 0){
+        console.log('input not filled');
+        return false
+    }
+
     return false
 }
 
-// btnSearch.onclick  = function(){
-//    if (arrayMovies.length > 0){
 
-//     textList.innerHTML = 'Films'
-//     textList.classList.add('d-flex','justify-content-center')
-//     sectFirst.appendChild(textList)  
-
-
-//     }else{
-//         textList.innerHTML = 'Sorry, no such movie'
-//         textList.classList.add('d-flex','justify-content-center')
-//         sectFirst.appendChild(textList)
-//     }
-//     // console.log(arrayMovies);
-//     // console.log(arrayMovies.length);
-// }
 
 
 
