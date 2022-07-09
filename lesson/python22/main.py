c277e57280d7697b2ccfd579da5f859116f30fc3
Alexey Golenko <alexey.holenko@gmail.com>
2022-07-09 10:08:17 +0300

    remove rar

--- lesson/python22/main.py
+++ lesson/python22/main.py
@@ -108,44 +108,90 @@
 # от баланса, обязательно создать проверки на нулевой баланс/большую цену та др. В конце нужно указать что купил пользователь и
 # словами указать сумму с правильным окончанием. Например: Ви купили Лате, ваша сдача: 7431 – семь тысяч четыреста тридцать один
 # доллар, или 2149 – две тысячи сто сорок девять долларов, 15 – пятнадцать долларов, 3 – три доллара
+#
+# num2words1 = {1: 'One', 2: 'Two', 3: 'Three', 4: 'Four', 5: 'Five', \
+#             6: 'Six', 7: 'Seven', 8: 'Eight', 9: 'Nine', 10: 'Ten', \
+#             11: 'Eleven', 12: 'Twelve', 13: 'Thirteen', 14: 'Fourteen', \
+#             15: 'Fifteen', 16: 'Sixteen', 17: 'Seventeen', 18: 'Eighteen', 19: 'Nineteen'}
+# num2words2 = ['Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety']
+#
+# def number(bal):
+#     if 1 <= bal < 19:
+#         return num2words1[bal]
+#     elif 20 <= bal <= 99:
+#         tens = bal//10
+#         below_ten = bal%10
+#         if below_ten == 0:
+#             return num2words2[tens-2]
+#         else: return num2words2[tens-2] + '-' + num2words1[below_ten]
+#     else:
+#         print("Number out of range")
+#
+#
+#
+# balans = input("Enter your balans: ")  # баланс от 0 до 100
+# print(f'Your balanse:{balans}$, please make a choice')
+# print(f'Num 1: Americano - 10$\n'
+#       f'Num 2: Americano with milk - 12$\n'
+#       f'Num 3: Latte - 15$\n'
+#       f'Num 4: Cappuccino -20$\n'
+#       f'Num 5: Espresso- 6%\n')
+#
+# choice = int(input("Choice: "))
+#
+# if not choice or len(balans) > 3 or 1 <= choice >6: print('You don\'t make a choice')
+# else:
+#     if int(balans) > choice:
+#          if choice == 1: print(f'your order: Americano\nyour change: {int(balans) -10}-{number(int(balans) -10)}')
+#          elif choice == 2: print(f'your order:Americano with milk\nyour change: {int(balans) -12}-{number(int(balans) -12)}')
+#          elif choice == 3: print(f'your order: Latte\nyour change: {int(balans) -15}-{number(int(balans) -15)}')
+#          elif choice == 4: print(f'your order: Cappuccino\nyour change: {int(balans) -20}-{number(int(balans) -20)}')
+#          elif choice == 5: print(f'your order: Espresso\nyour chang: {int(balans) - 6}-{number(int(balans) - 6)}')
+#     else: print('you poor')
 
-num2words1 = {1: 'One', 2: 'Two', 3: 'Three', 4: 'Four', 5: 'Five', \
-            6: 'Six', 7: 'Seven', 8: 'Eight', 9: 'Nine', 10: 'Ten', \
-            11: 'Eleven', 12: 'Twelve', 13: 'Thirteen', 14: 'Fourteen', \
-            15: 'Fifteen', 16: 'Sixteen', 17: 'Seventeen', 18: 'Eighteen', 19: 'Nineteen'}
-num2words2 = ['Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety']
 
-def number(bal):
-    if 1 <= bal < 19:
-        return num2words1[bal]
-    elif 20 <= bal <= 99:
-        tens = bal//10
-        below_ten = bal%10
-        if below_ten == 0:
-            return num2words2[tens-2]
-        else: return num2words2[tens-2] + '-' + num2words1[below_ten]
-    else:
-        print("Number out of range")
+#/--------------------LESSON3--------------------/
+# fact = 1
+# num = 5
+# i = 1
+# while i <= num:
+#     print(f'Iteration #{i}, fact: {fact}')
+#     fact *= i
+#     i += 1
+#
+# print(fact)
 
+# while True:
+#     answer = input('Enter 0 to stop: ')
+#     if answer == '0':
+#         break
 
 
-balans = input("Enter your balans: ")  # баланс от 0 до 100
-print(f'Your balanse:{balans}$, please make a choice')
-print(f'Num 1: Americano - 10$\n'
-      f'Num 2: Americano with milk - 12$\n'
-      f'Num 3: Latte - 15$\n'
-      f'Num 4: Cappuccino -20$\n'
-      f'Num 5: Espresso- 6%\n')
+# num = 0
+# max = 0
+# min = 0
+# sum = 0
+# count = 0
+# while True:
+#
+#     num = int(input('Enter num '))
+#     if num == 7: break
+#
+#     if min > num or count == 0:
+#         min = num
+#     if max < num: max=num
+#     sum += num
+#     count +=1
+#
+# print(f'Sum: {sum}\nMax: {max}\nMin: {min}')
+# print('bye')
 
-choice = int(input("Choice: "))
-
-if not choice or len(balans) > 3 or 1 <= choice >6: print('You don\'t make a choice')
-else:
-    if int(balans) > choice:
-         if choice == 1: print(f'your order: Americano\nyour change: {int(balans) -10}-{number(int(balans) -10)}')
-         elif choice == 2: print(f'your order:Americano with milk\nyour change: {int(balans) -12}-{number(int(balans) -12)}')
-         elif choice == 3: print(f'your order: Latte\nyour change: {int(balans) -15}-{number(int(balans) -15)}')
-         elif choice == 4: print(f'your order: Cappuccino\nyour change: {int(balans) -20}-{number(int(balans) -20)}')
-         elif choice == 5: print(f'your order: Espresso\nyour chang: {int(balans) - 6}-{number(int(balans) - 6)}')
-    else: print('you poor')
-
+num = int(input('Enter size: '))
+for x in range(1,4):
+        for j in range(1, 5):
+                print('*'*num + '_'*num,end='')
+        print('\n')
+for x in range(1, 4):
+        for j in range(1, 5):
+                print('_' * num + '*' * num, end='')
+        print('\n')
