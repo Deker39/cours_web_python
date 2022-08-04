f88677fe7a2b0f835cbe61817765342f6ccf9392
Alexey Golenko <alexey.holenko@gmail.com>
2022-08-04 23:04:46 +0300

    16

--- practice_16_js/pw.js
+++ practice_16_js/pw.js
@@ -1,7 +1,7 @@
 document.getElementById('randBtn').onclick = function(){
     document.querySelector('#randNumber').innerHTML  = Math.floor(Math.random()*100)
 }
-
+/*-------------------------------------------------*/
 var xy = document.createElement('h5')
 var mouseClick = document.createElement('h5')
 var sect = document.querySelector('#srceen')
@@ -19,7 +19,7 @@ sect.oncontextmenu = function(){
 sect.onmousemove = function(event){
     xy.innerHTML = `x = ${event.clientX} ,y = ${event.clientY}`
 }
-
+/*-------------------------------------------------*/
 function btnHide(){
     var text = document.querySelector('#text')
     if(text.style.display == 'block'){
@@ -32,3 +32,56 @@ function btnHide(){
     
     
 }
+/*-------------------------------------------------*/
+function openCity(e, cityName) {
+    var i, 
+    tabcontent, 
+    tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    tablinks = document.getElementsByClassName("tablinks");
+
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace("active", "");
+    }
+    
+    document.getElementById(cityName).style.display = "block";
+    e.currentTarget.className += "active";
+}
+
+window.onload = function() {
+    openCity(event,'html');
+    document.querySelector('#first').classList.add('active')
+  };
+
+/*-------------------------------------------------*/
+removes  = document.querySelectorAll('#remove')
+list = document.querySelector('#listIpsum')
+
+for (i = 0; i < list.children.length; i++){
+    removes[i].onclick = function(){
+        this.parentNode.remove()
+    }
+}
+
+
+/*-------------------------------------------------*/
+var width = 0;
+var i = 0;
+document.getElementById('add').onclick = function(){
+    var elem = document.querySelector('.progress-bar');
+    if (width >= 100) {
+
+    } else {
+    width += 5;
+    elem.style.width = width + "%";
+    elem.innerHTML = width  + "%";
+    }
+    
+
+}
+
