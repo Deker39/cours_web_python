3dc9cd0098ad3d4141a783abc37db8082d92e895
Alexey Golenko <alexey.holenko@gmail.com>
2022-08-08 22:17:18 +0300

    17 и 18

--- practice_17_js/pw.js
+++ practice_17_js/pw.js
@@ -22,5 +22,50 @@ like.onclick = function(){
 
 }
 /*-------------------------------------------------*/
+const equal = document.querySelector('.btn-equal')
+const numbers = document.querySelectorAll('.btn-number')
+const sing = document.querySelectorAll('.btn-sing')
+const vvod = document.querySelector('.vvod')
+
+let result = new String()
+
+numbers.forEach( e =>{
+    e.onclick = function(){
+        console.log(e.textContent);
+        result += e.textContent
+        vvod.textContent == '0'?  vvod.innerHTML =  e.textContent: vvod.textContent += e.textContent
+    }
+})
+sing.forEach( e =>{
+    e.onclick = function(){
+        console.log(e.textContent);
+        result += e.textContent
+        vvod.textContent == '0'?  vvod.innerHTML =  e.textContent: vvod.textContent += e.textContent
+    }
+})
+
+equal.onclick = function(){
+   console.log(eval(result));
+   vvod.textContent = eval(result)
+   setTimeout(() =>  vvod.innerHTML = '0', 2000);
+}
 /*-------------------------------------------------*/
+const list = document.getElementById('list')
+
+list.addEventListener('mouseover',function(e) {
+    e.target.children[0].classList.toggle('display-block')
+})
+
 /*-------------------------------------------------*/
+
+const scroll = document.querySelector('.scrollup')
+
+
+scroll.onclick = function(){
+    window.scrollTo(0, 0) 
+}
+
+window.addEventListener('scroll', function() {
+    pageYOffset > 100? scroll.style.display = 'block':scroll.style.display = 'none'
+
+})
