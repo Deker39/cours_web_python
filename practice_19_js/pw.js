03cbf5f11c19a7bbbd56b9a2f15d34f0d8cc6593
Alexey Golenko <alexey.holenko@gmail.com>
2022-08-18 00:01:27 +0300

    1

--- practice_19_js/pw.js
+++ practice_19_js/pw.js
@@ -88,4 +88,70 @@ let table = document.createElement('table')
     document.getElementById('thirdSection').appendChild(table)
 
 /*-------------------------------------------------*/
+function creatRGBItem(colorRed,colorGreen,colorBlue){
+    let contRGB = document.getElementById('contRGB')
+    let divitem =  document.createElement('div')
+    let divColor = document.createElement('div')
+    let hName = document.createElement('h5')
+    
+    divitem.classList.add('border', 'd-flex', 'align-items-center', 'm-3')
+    divColor.classList.add('cont')
+    hName.classList.add('p-2', 'pe-4', 'ps-3')
+
+    divColor.style.backgroundColor = `RGB(${colorRed},${colorGreen},${colorBlue})`
+
+    hName.textContent = `RGB(${colorRed}, ${colorGreen}, ${colorBlue})`
+
+    contRGB.appendChild(divitem)
+    divitem.appendChild(divColor)
+    divitem.appendChild(hName)
+}
+
+const rgbForm = document.getElementById('fourth'),
+    red = document.getElementById('red'),
+    green = document.getElementById('green'),
+    blue = document.getElementById('blue'),
+    inputRGB = document.querySelectorAll('input[name="rgb"]')
+let counter = 0
+
+rgbForm.onsubmit = function(){
+
+    inputRGB.forEach(function (input){
+        if(input.value === '' || input.value === ' ' || input.value < 0 || input.value > 255){
+            input.classList.add('is-invalid')
+            return false
+        }else{
+            input.classList.remove('is-invalid')
+            input.classList.add('is-valid')
+        }  
+    })
+
+    if(!red.value ||  !green.value || !blue.value || !red.value && !green.value && !blue.value ){
+            return false
+        }
+        else{
+            if(red.value < 0 || red.value > 255 || green.value < 0 || green.value > 255 || blue.value < 0 || blue.value > 255){
+                return false
+            }else{
+
+                let colorkey = "color" + counter;
+                counter++
+
+                document.cookie = `count=${colorkey}; red=${encodeURIComponent(red.value.toString())};
+                 green=${encodeURIComponent(green.value.toString())}; blue=${encodeURIComponent(blue.value.toString())};path=/`
+                document.cookie = `counter=${counter};path=/`;
+                //пределать на обьект для отправки в куки 
+                // creatRGBItem(red.value,green.value,blue.value)
+            }
+  
+        }
+
+    
+    
+
+    return false
+}
+let cookieString = decodeURIComponent(document.cookie);
+let cookieArray = cookieString.split("; ")
+console.log(cookieArray);
 /*-------------------------------------------------*/
