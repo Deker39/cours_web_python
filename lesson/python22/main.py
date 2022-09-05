69174678a413999064ac1976b1c3e6eae2ca9538
Alexey Golenko <alexey.holenko@gmail.com>
2022-09-05 23:33:56 +0300

    34

--- lesson/python22/main.py
+++ lesson/python22/main.py
@@ -1168,14 +1168,36 @@ import time
 #     return statr
 
 # @get_diff
-@functools.lru_cache
-def get_data(n):
-    s = time.time()
+# @functools.lru_cache
+# def get_data(n):
+#     s = time.time()
+#
+#     diff = time.time() - s
+#     print(f'fact {math.factorial(n)}')
+#     print(diff)
+#
+# while True:
+#     n = int(input('s: '))
+#     get_data(n)
 
-    diff = time.time() - s
-    print(f'fact {math.factorial(n)}')
-    print(diff)
+numbers = [5,2,4,7,6]
 
-while True:
-    n = int(input('s: '))
-    get_data(n)
+
+def myBubbleSort(myList):
+    for i in range(len(myList)-1):
+        for j in range(len(myList)-i-1):
+            if myList[j] < myList[i]:
+                temp=myList[j]
+                myList[j]=myList[j+1]
+                myList[j+1]= temp
+                # myList[j], myList[j + 1] = myList[j + 1], myList[j]
+
+def printList(myList):
+    for index, elem in enumerate(myList):
+        print('element {}: {}'.format(index+1,elem))
+
+print("Original list:")
+printList(numbers)
+myBubbleSort(numbers)
+print("Sorted list:")
+printList(numbers)
