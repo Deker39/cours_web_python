6bc71de61bc51adcc30f46ffdc68a5bcc587c033
Alexey Golenko <alexey.holenko@gmail.com>
2022-09-25 19:25:32 +0300

    21

--- practice/practice_21_js_here/pw.js
+++ practice/practice_21_js_here/pw.js
@@ -1,4 +1,24 @@
 
 $('#btn').click(function(){
-    $('#floatingTextarea2').text(JSON.stringify((JSON.parse($('#floatingTextarea1').val())), 'null', 4))
+    // {"name":"John", "age":30, "car":null}
+    try{
+        $('#floatingTextarea2').text(JSON.stringify((JSON.parse($('#floatingTextarea1').val())), 'null', 4))
+        $('#floatingTextarea1').addClass('is-valid')
+        $('#floatingTextarea1').removeClass('is-invalid')
+    }
+    catch (e){
+        $('#floatingTextarea1').addClass('is-invalid')
+        $('#floatingTextarea1').removeClass('is-valid')
+        $('#floatingTextarea2').text = ''
+    }
+   
 }) 
+
+let user = new Array()
+
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(data => user.push(data))
+
+  console.log(user);
+
