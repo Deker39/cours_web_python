5fb3b422bf8f2b6a3f51351ab79099fd3a427315
Alexey Golenko <alexey.holenko@gmail.com>
2022-10-04 22:54:40 +0300

    21

--- practice/practice_21_js_here/pw.js
+++ practice/practice_21_js_here/pw.js
@@ -1,3 +1,18 @@
+class User {
+    constructor(id, name, username, phone, email, company, address, website) {
+      (this.id = id),
+        (this.name = name),
+        (this.username = username),
+        (this.phone = phone),
+        (this.email = email),
+        (this.company = company),
+        (this.address = address),
+        (this.website = website);
+    }
+  }
+  
+  var listUsers = Array();
+
 
 $('#btn').click(function(){
     // {"name":"John", "age":30, "car":null}
@@ -13,35 +28,84 @@ $('#btn').click(function(){
     }
    
 }) 
-async function Test(){
+
+async function loadFetch(){
     const res = await fetch('https://jsonplaceholder.typicode.com/users')
     return res
 }
-let user = new Array()
 
 
-//   .then(response => response.json())
-//   .then(item => item.forEach(i => {user.push(i)}))
-
-Test().then(response => response.json())
-    .then(item => item.forEach(i => {user.push(i)}))
 
 
-console.log(user)
+async function loadFetch() {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const body = await res.json();
+    if (body) {
+        listUsers = body.map(
+        (i) =>
+          new User(
+            i.id,
+            i.name,
+            i.username,
+            i.phone,
+            i.email,
+            i.company,
+            i.address,
+            i.website
+          )
+      );
 
-// console.log()
+      listUsers.forEach((e) => {
+        $("#contCard").append(
+          $(`<div class="border col-3 p-3 pe-4 m-3" id="user${e.id}"><h5>${e.name}</h5></div>`)
+        );
+      });
+    }
+  }
 
-// for (let i = 0; i < user.length; i++) {
-//     console.log(user[i]['name']);
-    
-// }
+loadFetch()
 
-user.forEach(e => {
-    console.log(e);
-});
-
-
-$('#contCard').append($(`<div class="border p-3 pe-4 m-3"><h4>'kek'</h4></div>`))
+$("#contCard").on('click','div', function(){
+    console.log(($( this ).attr('id')).replace('user',''));
+    let selectedUser = ($( this ).attr('id')).replace('user','')-1
+    console.log(listUsers[selectedUser].name);
+    $('body').append(
+        $(`
+        <section class="container d-flex align-items-center justify-content-center  flex-column">
+            <h5>All users:</h5>
+            <table class="table table-responsive table-bordered ">
+                <tbody>
+                    <tr>
+                    <th scope="row" class=" ">Name:</th>
+                    <td class="ps-4">${listUsers[selectedUser].name}</td>
+                    </tr>
+                    <tr>
+                    <th scope="row" class=" ">Username:</th>
+                    <td class="ps-4">${listUsers[selectedUser].username}</td>
+                    </tr>
+                    <tr>
+                    <th scope="row" class=" ">Address:</th>
+                    <td class="ps-4">${listUsers[selectedUser].address}</td>
+                    </tr>
+                    <tr>
+                    <th scope="row" class=" ">Email:</th>
+                    <td class="ps-4">${listUsers[selectedUser].email}</td>
+                    </tr>
+                    <tr>
+                    <th scope="row" class=" ">Phone:</th>
+                    <td class="ps-4">${listUsers[selectedUser].phone}</td>
+                    </tr>
+                    <tr>
+                    <th scope="row" class=" ">Website:</th>
+                    <td class="ps-4">${listUsers[selectedUser].website}</td>
+                    </tr>
+                </tbody>
+                </table>
+                
+        </section>`
+        )
+    )
+})
 
 
 
