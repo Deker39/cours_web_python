76b18cf4d26172281e92c3825092d0d9af3291a5
Alexey Golenko <alexey.holenko@gmail.com>
2022-11-26 22:04:45 +0200

    56

--- home_work/lesson_56_hw_python/hw.py
+++ home_work/lesson_56_hw_python/hw.py
@@ -28,7 +28,7 @@ class Stack:
         self.top = self.top.n
         self.size -= 1
 
-    def size(self):
+    def size_stack(self):
         return self.size
 
     def empty(self):
@@ -39,13 +39,14 @@ class Stack:
 
     def clear(self):
         self.top = None
+        self.size = 0
         # self.top.n = None
 
     def get_item(self):
         if not self.empty():
             return self.top.i
         else:
-            print('Stavk empty')
+            print('Stack empty')
 
     def travel(self):
         cur = self.top
@@ -57,45 +58,61 @@ class Stack:
 
 
 s = Stack()
-s.push(5)
-s.push(10)
-s.push(15)
-s.travel()
-s.pop()
-# s.push(20)
 
-s.travel()
+def print_menu(func):
 
-#
-# print('Work with the stack')
-# print('1.Add in stack\n'
-#       '2.Push from stack\n'
-#       '3.Size the stack\n'
-#       '4.Check for emptiness\n'
-#       '5.check for fulless\n'
-#       '6.Clear  stack\n'
-#       '7.Get item\n'
-#       '8.Exit')
-#
-# choose = int(input('Choose: '))
-#
-# while choose != 8:
-#
-#     if choose == 1:
-#         pass
-#     elif choose == 2:
-#         pass
-#     elif choose == 3:
-#         pass
-#     elif choose == 3:
-#         pass
-#     elif choose == 4:
-#         pass
-#     elif choose == 5:
-#         pass
-#     elif choose == 6:
-#         pass
-#     elif choose == 7:
-#         pass
-#     else:
-#         print('Inccorect value')
+    def inner():
+        print('\nWork with the stack\n')
+        print('1.Add in stack\n'
+              '2.Push from stack\n'
+              '3.Size the stack\n'
+              '4.Check for emptiness\n'
+              '5.Check for fulless\n'
+              '6.Clear  stack\n'
+              '7.Get item\n'
+              '8.Show stack\n'
+              '9.Exit\n')
+        return func()
+
+    return inner
+
+@print_menu
+def menu():
+    return int(input('Choose: '))
+
+
+choose = menu()
+
+while choose != 9:
+
+    if choose == 1:
+        s.push(input('Enter your value: '))
+        s.travel()
+        choose = menu()
+    elif choose == 2:
+        print('The last element was a delete from stack')
+        s.pop()
+        s.travel()
+        choose = menu()
+    elif choose == 3:
+        s.size_stack()
+        choose = menu()
+    elif choose == 4:
+        print(f'Empty: {s.empty()}')
+        choose = menu()
+    elif choose == 5:
+        print(f'Full: {s.full()}')
+        choose = menu()
+    elif choose == 6:
+        s.clear()
+        choose = menu()
+    elif choose == 7:
+        print(s.get_item())
+        choose = menu()
+    elif choose == 8:
+        s.travel()
+        choose = menu()
+    else:
+        print('Inccorect value')
+
+#Если еще надо третье задание то я доделаю оно не сложное
